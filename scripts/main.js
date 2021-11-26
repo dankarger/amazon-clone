6edd5d4e0755bfbc1b89927f147f919f7316cf40
dankarger <karger.dan@gmail.com>
2021-11-27 00:00:23 +0200

    trying to make order, add data,js

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,5 @@
+import {productsDataBase} from "./data.js";
+
 let mainImage = document.querySelector('.main-image')
 const smallImage1 = document.querySelector('.small-img1')
 const smallImage2 = document.querySelector('.small-img2')
@@ -66,76 +68,75 @@ function removeCart() {
 //     }
 // }
 
-const productsDataBase = [
-    {
-        img: "../assets/images/41lRhUCEBOL._AC_SR160,160_.jpg",
-        text: 'hi D Z Strad Carbon Fiber Cello Bow with Traditional Frog made from Polished Premium Ebony 4/4 Full Size\n' +
-            'D Z Strad Carbon Fiber Cello Bow with Traditional Frog made from Polished…\n',
-        stars: 'stars',
-        rating: '16',
-        price: '$129.00',
-        prime: 'prime'
-    },
-    {
-        img: "../assets/images/61K1RfassZS._AC_UL160_SR160,160_.jpg",
-        text: 'hi2 Aileen 4/4 Full Size Burgundy Wooden Cello Box Stand, Black Premium Velvet Plush Interior Including Bow Holder with Anti-Slip Mat\n' +
-            'Aileen 4/4 Full Size Burgundy Wooden Cello Box Stand, Black Premium Velvet Plush…\n',
-        stars: 'stars',
-        rating: '51',
-        price: '$145.99',
-        prime: 'prime'
-    }, {
-        img: "../assets/images/41j7-wCYxnL._AC_SR160,160_.jpg",
-        text: 'hi3 Cello Stand Adjustable, Folding Cello Support Stand, A-Frame Folding Cello Holder Compatible for Violin 1/8-4/4 Cellos Guitars Electric Bass Electric Guitar Stand Acoustic, Black\n' +
-            'Cello Stand Adjustable, Folding Cello Support Stand, A-Frame Folding Cello Holder C...\n',
-        stars: 'stars',
-        rating: '10',
-        price: '$30.99',
-        prime: 'prime'
-    },
-    {
-        img: "../assets/images/41VQomgeOIL._AC_SR160,160_.jpg",
-        text: 'Cello Strings 1 Full Set A-D-G-C Steel Core Nickel Chromium Wound for Size 1/4 1/2 3/4 4/4\n' +
-            'Cello Strings 1 Full Set A-D-G-C Steel Core Nickel Chromium Wound for Size 1/4 1/2 ...\n',
-        stars: 'stars',
-        rating: '110',
-        price: '$13.99',
-        prime: 'prime'
-    }, {
-        img: "../assets/images/51k7rvaYVcL._AC_SR160,160_ (1).jpg",
-        text: 'Touch of Class Ayden Music Stand Windsor Oak One Size\n' +
-            'Touch of Class Ayden Music Stand Windsor Oak One Size\n',
-        stars: 'stars2',
-        rating: '2',
-        price: '$169.00',
-        prime: 'prime'
-    },
-    {
-        img: "../assets/images/51k7rvaYVcL._AC_SR160,160_ (1).jpg",
-        text: 'Hiiiii Touch of Class Ayden Music Stand Windsor Oak One Size\n' +
-            'Touch of Class Ayden Music Stand Windsor Oak One Size\n',
-        stars: 'stars2',
-        rating: '2',
-        price: '$169.00',
-        prime: 'prime'
-    },{
-        img: "../assets/images/41lRhUCEBOL._AC_SR160,160_.jpg",
-        text: 'D Z Strad Carbon Fiber Cello Bow with Traditional Frog made from Polished Premium Ebony 4/4 Full Size\n' +
-            'D Z Strad Carbon Fiber Cello Bow with Traditional Frog made from Polished…\n',
-        stars: 'stars',
-        rating: '16',
-        price: '$129.00',
-        prime: 'prime'
-    },{
-        img: "../assets/images/41lRhUCEBOL._AC_SR160,160_.jpg",
-        text: 'Heeeloo w with Traditional Frog made from Polished Premium Ebony 4/4 Full Size\n' +
-            'D Z Strad Carbon Fiber Cello Bow with Traditional Frog made from Polished…\n',
-        stars: 'stars',
-        rating: '16',
-        price: '$129.00',
-        prime: 'prime'
-    }
-]
+// const productsDataBase = [
+//     {
+//         img: "../assets/images/41lRhUCEBOL._AC_SR160,160_.jpg",
+//         text: 'hi D Z Strad Carbon Fiber Cello Bow with Traditional Frog made from Polished Premium Ebony 4/4 Full Size\n' +
+//             'D Z Strad Carbon Fiber Cello Bow with Traditional Frog made from Polished…\n',
+//         stars: 'stars',
+//         rating: '16',
+//         price: '$129.00',
+//         prime: 'prime'
+//     },
+//     {
+//         img: "../assets/images/61K1RfassZS._AC_UL160_SR160,160_.jpg",
+//         text: 'hi2 Aileen 4/4 Full Size Burgundy Wooden Cello Box Stand, Black Premium Velvet Plush Interior Including Bow Holder with Anti-Slip Mat\n' +
+//             'Aileen 4/4 Full Size Burgundy Wooden Cello Box Stand, Black Premium Velvet Plush…\n',
+//         stars: 'stars',
+//         rating: '51',
+//         price: '$145.99',
+//         prime: 'prime'
+//     }, {
+//         img: "../assets/images/41j7-wCYxnL._AC_SR160,160_.jpg",
+//         text: 'hi3 Cello Stand Adjustable, Folding Cello Support Stand, A-Frame Folding Cello Holder Compatible for Violin 1/8-4/4 Cellos Guitars Electric Bass Electric Guitar Stand Acoustic, Black\n' +
+//             'Cello Stand Adjustable, Folding Cello Support Stand, A-Frame Folding Cello Holder C...\n',
+//         stars: 'stars',
+//         rating: '10',
+//         price: '$30.99',
+//         prime: 'prime'
+//     },
+//     {
+//         img: "../assets/images/41VQomgeOIL._AC_SR160,160_.jpg",
+//         text: 'Cello Strings 1 Full Set A-D-G-C Steel Core Nickel Chromium Wound for Size 1/4 1/2 3/4 4/4\n' +
+//             'Cello Strings 1 Full Set A-D-G-C Steel Core Nickel Chromium Wound for Size 1/4 1/2 ...\n',
+//         stars: 'stars',
+//         rating: '110',
+//         price: '$13.99',
+//         prime: 'prime'
+//     }, {
+//         img: "../assets/images/51k7rvaYVcL._AC_SR160,160_ (1).jpg",
+//         text: 'Touch of Class Ayden Music Stand Windsor Oak One Size\n' +
+//             'Touch of Class Ayden Music Stand Windsor Oak One Size\n',
+//         stars: 'stars2',
+//         rating: '2',
+//         price: '$169.00',
+//         prime: 'prime'
+//     },
+//     {
+//         img: "../assets/images/51k7rvaYVcL._AC_SR160,160_ (1).jpg",
+//         text: 'Hiiiii Touch of Class Ayden Music Stand Windsor Oak One Size\n' +
+//             'Touch of Class Ayden Music Stand Windsor Oak One Size\n',
+//         stars: 'stars2',
+//         rating: '2',
+//         price: '$169.00',
+//         prime: 'prime'
+//     },{
+//         img: "../assets/images/41lRhUCEBOL._AC_SR160,160_.jpg",
+//         text: 'D Z Strad Carbon Fiber Cello Bow with Traditional Frog made from Polished Premium Ebony 4/4 Full Size\n' +
+//             'D Z Strad Carbon Fiber Cello Bow with Traditional Frog made from Polished…\n',
+//         stars: 'stars',
+//         rating: '16',
+//         price: '$129.00',
+//         prime: 'prime'
+//     },{
+//         img: "../assets/images/51-ZytUJP2L._AC_UL160_SR160,160_.jpg",
+//         text: 'Vio Music Cello Wooden Stand Burgundy Velvet Plush Cushions\n',
+//         stars: 'stars',
+//         rating: '121',
+//         price: '$169.00',
+//         prime: 'prime'
+//     }
+// ]
 
 
 function injectCard(card) {
