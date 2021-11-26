4fc41c9522ace03b56e9d76abf4810b6ce08a824
dankarger <karger.dan@gmail.com>
2021-11-26 18:00:05 +0200

    fix problem with sidescrolling

--- scripts/main.js
+++ scripts/main.js
@@ -4,15 +4,15 @@ const smallImage1 = document.querySelector('.small-img1')
 const smallImage2 = document.querySelector('.small-img2')
 const smallImage3 = document.querySelector('.small-img3')
 const smallImagesArray = [smallImage1, smallImage2, smallImage3]
-
-
-
+let cartNumber = document.querySelector('.cart-number')
+const addCartButton = document.querySelector('.btn-add')
+const buyNowButton = document.querySelector('.btn-buy')
 
 smallImage1.addEventListener('mouseover',()=>changeImage('../assets/images/91k6YqnolGL._AC_SX679_.jpg', smallImage1))
 smallImage2.addEventListener('mouseover',()=>changeImage('../assets/images/61LaT97MXJL._AC_SX679_.jpg',smallImage2))
-
 smallImage3.addEventListener('mouseover',()=>changeImage('../assets/images/51d3jUFuuoL._AC_SX679_.jpg',smallImage3))
-
+addCartButton.addEventListener('click',()=>{addCart()})
+buyNowButton.addEventListener('click', ()=>{removeCart()})
 
 function changeImage(image,imageElement){
     smallImagesArray.forEach(el=>{
@@ -23,5 +23,19 @@ function changeImage(image,imageElement){
     imageElement.style.borderColor='#f2672c'
     return mainImage.src=image
 
+}
 
+function addCart() {
+    const qtyNumber = parseInt(document.getElementById('qty-number').value)
+    console.log(qtyNumber)
+    let prevValue = parseInt(cartNumber.innerHTML)
+    let mewNumber  =prevValue + qtyNumber
+    prevValue<50?cartNumber.innerHTML=mewNumber:cartNumber.innerHTML=prevValue
+
+}
+
+function removeCart() {
+    let prevValue = parseInt(cartNumber.innerHTML)
+
+    prevValue>0?cartNumber.innerHTML--:cartNumber.innerHTML = '0';
 }
