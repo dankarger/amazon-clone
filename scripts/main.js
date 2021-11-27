bebf2cee026fa46d5717784341dd87458acc22b5
dankarger <karger.dan@gmail.com>
2021-11-27 03:36:45 +0200

    add page of

--- scripts/main.js
+++ scripts/main.js
@@ -12,13 +12,19 @@ const addCartButton = document.querySelector('.btn-add')
 const buyNowButton = document.querySelector('.btn-buy')
 let carouselleftButton = document.querySelector('.chevron-left-big-icon')
 let carouselRightButton = document.querySelector('.chevron-right-big-icon')
+let currentPage = document.querySelector('.page')
+let restOfPages = document.querySelector('.restOfpages')
 let pageItems = 1
 let width = window.innerWidth
-if(width<600)pageItems=1
-if(width<800&&width>600)pageItems=2
-if(width<1100&&width>800)pageItems=3
-if(width<1300&&width>1100)pageItems=4
-if(width>1300)pageItems=6
+checkWidthAtRefresh(width)
+
+function checkWidthAtRefresh(width) {
+    if (width < 600) pageItems = 1
+    if (width < 800 && width > 600) pageItems = 2
+    if (width < 1100 && width > 800) pageItems = 3
+    if (width < 1300 && width > 1100) pageItems = 4
+    if (width > 1300) pageItems = 6
+}
 // checkWidth(width)
 
 smallImage1.addEventListener('mouseover', () => changeImage('../assets/images/91k6YqnolGL._AC_SX679_.jpg', smallImage1))
@@ -44,17 +50,23 @@ function changeImage(image, imageElement) {
 // if(width<1000){pageItems=3}
 let pages = paginator(productsDataBase,pageItems)
 
-
+currentPage.innerHTML=pageNumber+1
+restOfPages.innerHTML =  pages.length
 function createCarousel(pages,pageNumber) {
      const cards=pages[pageNumber]
       injectRowOfCards(cards)
 }
 
 function pageChange(value,pages,pageNumber){
+
    let newPageNumber= pageSelector(value,pages)
     if(newPageNumber===pageNumber) {
+        currentPage.innerHTML=pageNumber+1
+        restOfPages.innerHTML = pages.length
         return
     }
+    currentPage.innerHTML=pageNumber+1
+    restOfPages.innerHTML = pages.length
     createCarousel(pages, newPageNumber)
 }
 
